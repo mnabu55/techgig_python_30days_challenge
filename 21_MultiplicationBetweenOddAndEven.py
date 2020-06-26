93ba268f857754d292dc9a430cbe265367aa1a97
Manabu Kiyota <manabu.kiyota@unisys.co.jp>
2020-06-27 07:29:30 +0900

    Add files via upload

---
+++ 21_MultiplicationBetweenOddAndEven.py
@@ -0,0 +1,33 @@
+'''
+Multiplication between odd and even (100 Marks)
+
+For this challenge, you need to take number of elements as input on one line and array elements as an input on another line. You need to find the numbers that are odd, add them. find the numbers that are even add them and then multiply the two values that you get after addition of even numbers and that of addition of odd numbers.
+
+Input Format
+In this challenge, you will take number of elements as input on one line and array elements which are space separated as input on another line.
+
+Constraints
+1<  N < 10^5
+1 < A[i] < 10^5
+
+Output Format
+You will print the value after multiplication to the stdout.
+
+Sample TestCase 1
+Input
+6
+11 22 33 44 55 66
+Output
+13068
+'''
+
+
+def main():
+    number = int(input())
+    array = list(map(int, input().split()))
+    even = [x for x in array if x % 2 == 0]
+    odd = [x for x in array if x % 2 != 0]
+    print(sum(even) * sum(odd), end="")
+
+
+main()
