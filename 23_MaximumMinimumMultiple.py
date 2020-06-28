071f23a2f1d3b3d2c6a31ed08693c08bfb61af1e
Manabu Kiyota <manabu.kiyota@unisys.co.jp>
2020-06-29 04:16:09 +0900

    Add files via upload

--- 23_MaximumMinimumMultiple.py
+++ 23_MaximumMinimumMultiple.py
@@ -1,3 +1,23 @@
+'''
+Maximum Vs Minimum (100 Marks)
+For this challenge, you need to take number of elements as input on one line and array elements as an input on another line. You need to find the minimum number and maximum number from the array and multiply them.
+
+Input Format
+In this challenge, you will take number of elements as input on one line and array elements which are space separated as input on another line.
+
+Constraints
+1 < N < 10^5
+1 < A[i] < 10^5
+
+Output Format
+You will print the value after multiplication to the stdout.
+
+Sample TestCase 1
+Input
+6
+11 22 33 44 55 66
+'''
+
 
 def main():
     n = int(input())
